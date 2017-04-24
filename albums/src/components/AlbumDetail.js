e640e26d8386325e41cd0fe6b3e32c21e7e8e35d
Erik S Carlsten <ecarlste@gmail.com>
2017-04-23 23:12:56 -0500

    Destructured the properties passed to the AlbumDetails and added Image

--- albums/src/components/AlbumDetail.js
+++ albums/src/components/AlbumDetail.js
@@ -3,17 +3,18 @@ import { Text, View, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 
-const AlbumDetail = (props) => {
+const AlbumDetail = ({ album }) => {
+    const { title, artist, thumbnail_image } = album;
+
     return (
         <Card>
             <CardSection>
                 <View>
-                    <Image />
+                    <Image source={{ uri: thumbnail_image }} />
                 </View>
-
                 <View style={styles.headerContentStyle}>
-                    <Text>{props.album.title}</Text>
-                    <Text>{props.album.name}</Text>
+                    <Text>{title}</Text>
+                    <Text>{artist}</Text>
                 </View>
             </CardSection>
         </Card>
