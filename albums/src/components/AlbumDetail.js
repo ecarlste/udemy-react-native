414ff28be830bf3e839788b23c0a0d534058e497
Erik S Carlsten <ecarlste@gmail.com>
2017-04-23 23:21:39 -0500

    Added a margin to the view surrounding the artist image

--- albums/src/components/AlbumDetail.js
+++ albums/src/components/AlbumDetail.js
@@ -5,12 +5,12 @@ import CardSection from './CardSection';
 
 const AlbumDetail = ({ album }) => {
     const { title, artist, thumbnail_image } = album;
-    const { thumbnailStyle, headerContentStyle } = styles;
+    const { thumbnailStyle, headerContentStyle, thumbnailContainerStyle } = styles;
 
     return (
         <Card>
             <CardSection>
-                <View>
+                <View style={thumbnailContainerStyle}>
                     <Image style={thumbnailStyle} source={{ uri: thumbnail_image }} />
                 </View>
                 <View style={headerContentStyle}>
@@ -30,6 +30,12 @@ const styles = {
     thumbnailStyle: {
         height: 50,
         width: 50
+    },
+    thumbnailContainerStyle: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 10,
+        marginRight: 10
     }
 };
 
