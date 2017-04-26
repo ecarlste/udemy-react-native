6099b48319bbca0312dfc9d09fb20d2fdf56f8c7
Erik S Carlsten <ecarlste@gmail.com>
2017-04-26 14:57:47 -0500

    Created a single index.js package for ease of use with reusable comps

--- auth/src/components/common/Button.js
+++ auth/src/components/common/Button.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Text, TouchableOpacity } from 'react-native';
 
-const Button = ({ onPress, children, title }) => {
+const Button = ({ onPress, children }) => {
     const { buttonStyle, textStyle } = styles;
 
     return (
         <TouchableOpacity onPress={onPress} style={buttonStyle}>
             <Text style={textStyle}>
-                {title}
+                {children}
             </Text>
         </TouchableOpacity>
     );
@@ -34,4 +34,4 @@ const styles = {
     }
 };
 
-export default Button;
+export { Button };
--- auth/src/components/common/Card.js
+++ auth/src/components/common/Card.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import { View } from 'react-native';
 
-const Card = (props) => {
-    return(
-        <View style={styles.containerStyle}>
-            {props.children}
-        </View>
-    );
-};
+const Card = (props) => (
+    <View style={styles.containerStyle}>
+        {props.children}
+    </View>
+);
 
 const styles = {
     containerStyle: {
@@ -16,7 +14,7 @@ const styles = {
         borderColor: '#ddd',
         borderBottomWidth: 0,
         shadowColor: '#000',
-        shadowOffset: { width: 0, height: 2},
+        shadowOffset: { width: 0, height: 2 },
         shadowOpacity: 0.1,
         shadowRadius: 2,
         elevation: 1,
@@ -26,4 +24,4 @@ const styles = {
     }
 };
 
-export default Card;
+export { Card };
