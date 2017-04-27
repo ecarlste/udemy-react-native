1ff85c4bfc5f12ee9a57e124958059b2b8759097
Erik S Carlsten <ecarlste@gmail.com>
2017-04-27 13:00:27 -0500

    Added firebase config init when App starts up

--- auth/src/app.js
+++ auth/src/app.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
 
 class App extends Component {
+    componentWillMount() {
+        firebase.initializeApp({
+                apiKey: 'AIzaSyCOLfjPmNbEP4q-VHU8WGaGLjV2DJKbNwY',
+                authDomain: 'authentication-f47fe.firebaseapp.com',
+                databaseURL: 'https://authentication-f47fe.firebaseio.com',
+                projectId: 'authentication-f47fe',
+                storageBucket: 'authentication-f47fe.appspot.com',
+                messagingSenderId: '790322251231'
+            });
+    }
+
     render() {
         return (
             <View>
