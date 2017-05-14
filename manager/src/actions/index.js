64a0e2eb70ecf9c7342e0c2b107368a281d9262b
Erik S Carlsten <ecarlste@gmail.com>
2017-05-14 13:58:30 -0600

    Added login user state type and error logging in loginUser action

--- manager/src/actions/index.js
+++ manager/src/actions/index.js
@@ -1,6 +1,7 @@
 import firebase from 'firebase';
 import {
     EMAIL_CHANGED,
+    LOGIN_USER,
     LOGIN_USER_FAIL,
     LOGIN_USER_SUCCESS,
     PASSWORD_CHANGED
@@ -22,9 +23,13 @@ export const passwordChanged = (text) => {
 
 export const loginUser = ({ email, password }) => {
     return (dispatch) => {
+        dispatch({ type: LOGIN_USER });
+
         firebase.auth().signInWithEmailAndPassword(email, password)
             .then(user => loginUserSuccess(dispatch, user))
-            .catch(() => {
+            .catch((error) => {
+                console.log(error);
+
                 firebase.auth().createUserWithEmailAndPassword(email, password)
                     .then(user => loginUserSuccess(dispatch, user))
                     .catch(() => loginUserFail(dispatch));
