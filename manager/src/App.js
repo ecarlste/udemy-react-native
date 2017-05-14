17e0028f54ad7aa19583b25ffa9ac28b7e5f5066
Erik S Carlsten <ecarlste@gmail.com>
2017-05-14 11:17:01 -0600

    Added firebase support to the app for user login via email

--- manager/src/App.js
+++ manager/src/App.js
@@ -2,9 +2,23 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
 import reducers from './reducers';
 
 class App extends Component {
+    componentWillMount() {
+        const config = {
+            apiKey: "AIzaSyCKu8rXiKbTQElTTVph5_-msQv4GWUeBRk",
+            authDomain: "manager-a6623.firebaseapp.com",
+            databaseURL: "https://manager-a6623.firebaseio.com",
+            projectId: "manager-a6623",
+            storageBucket: "manager-a6623.appspot.com",
+            messagingSenderId: "742825901539"
+        };
+
+        firebase.initializeApp(config);
+    }
+
     render() {
         return (
             <Provider store={createStore(reducers)}>
