c79eff78db5de2e034eb68a48d55ad5051a8ab52
Erik S Carlsten <ecarlste@gmail.com>
2017-05-15 17:14:51 -0600

    Added a picker component to allow employee shift days to be selected

--- manager/src/components/EmployeeCreate.js
+++ manager/src/components/EmployeeCreate.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Picker } from 'react-native';
 import { connect } from 'react-redux';
 import { employeeUpdate } from '../actions/EmployeeActions';
 import { Card, CardSection, Input, Button } from './common';
@@ -13,7 +14,7 @@ class EmployeeCreate extends Component {
                         placeholder="Jane"
                         value={this.props.name}
                         onChangeText={
-                            text => this.props.employeeUpdate({ prop: 'name', value: text })
+                            value => this.props.employeeUpdate({ prop: 'name', value })
                         }
                     />
                 </CardSection>
@@ -24,12 +25,26 @@ class EmployeeCreate extends Component {
                         placeholder="555-555-5555"
                         value={this.props.phone}
                         onChangeText={
-                            text => this.props.employeeUpdate({ prop: 'phone', value: text })
+                            value => this.props.employeeUpdate({ prop: 'phone', value })
                         }
                     />
                 </CardSection>
 
-                <CardSection />
+                <CardSection>
+                    <Picker
+                        style={{ flex: 1 }}
+                        selectedValue={this.props.shift}
+                        onValueChange={value => this.props.employeeUpdate({ prop: 'shift', value })}
+                    >
+                        <Picker.Item label="Monday" value="Monday" />
+                        <Picker.Item label="Tuesday" value="Tuesday" />
+                        <Picker.Item label="Wednesday" value="Wednesday" />
+                        <Picker.Item label="Thursday" value="Thursday" />
+                        <Picker.Item label="Friday" value="Friday" />
+                        <Picker.Item label="Saturday" value="Saturday" />
+                        <Picker.Item label="Sunday" value="Sunday" />
+                    </Picker>
+                </CardSection>
 
                 <CardSection>
                     <Button onPress={() => console.log('Save Pressed!')}>Save</Button>
