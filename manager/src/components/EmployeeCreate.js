7e577b272cda970bed4141c50e2c5b832fdb64bd
Erik S Carlsten <ecarlste@gmail.com>
2017-05-15 17:23:03 -0600

    Added text label to the Shift picker component on employee create form

--- manager/src/components/EmployeeCreate.js
+++ manager/src/components/EmployeeCreate.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Picker } from 'react-native';
+import { Picker, Text } from 'react-native';
 import { connect } from 'react-redux';
 import { employeeUpdate } from '../actions/EmployeeActions';
 import { Card, CardSection, Input, Button } from './common';
@@ -31,6 +31,7 @@ class EmployeeCreate extends Component {
                 </CardSection>
 
                 <CardSection>
+                    <Text style={styles.pickerLabelStyle}>Shift</Text>
                     <Picker
                         style={{ flex: 1 }}
                         selectedValue={this.props.shift}
@@ -53,6 +54,13 @@ class EmployeeCreate extends Component {
         );
     }
 }
+
+const styles = {
+    pickerLabelStyle: {
+        fontSize: 18,
+        paddingLeft: 20
+    }
+};
 
 const mapStateToProps = (state) => {
     const { name, phone, shift } = state.employeeForm;
